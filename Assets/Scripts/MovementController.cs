a6958edaf52fccd81778f1a6c278b384e2d3b3ee
iDoObject <19980290+iDoObject@users.noreply.github.com>
2020-02-11 11:34:07 +0100

    Initial Commit

---
+++ Assets/Scripts/MovementController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementController : MonoBehaviour
+{
+    public float fuel = 100;
+    public float rotationalThrust = 1;
+    public float translationalThrust = 1;
+
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float ventralTrans = Input.GetAxis("Ventral");
+        float lateralTrans = Input.GetAxis("Lateral");
+
+        rb.AddForce(new Vector3(lateralTrans * translationalThrust, 0, ventralTrans * translationalThrust));
+    }
+}
