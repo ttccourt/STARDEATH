6cd4718714e901c7c41d8a0023041dca0de8f7f1
iDoObject <19980290+iDoObject@users.noreply.github.com>
2020-02-14 13:12:20 +0100

    Add a constraint box

--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class MovementController : MonoBehaviour
 {
     public float fuel = 100;
-    public float rotationalThrust = 1;
-    public float translationalThrust = 1;
+    public float rotationalThrust;
+    public float translationalThrust;
 
     private Rigidbody rb;
 
@@ -20,7 +20,14 @@ public class MovementController : MonoBehaviour
     {
         float ventralTrans = Input.GetAxis("Ventral");
         float lateralTrans = Input.GetAxis("Lateral");
+        float cranialTrans = Input.GetAxis("Cranial");
+        float pitch = Input.GetAxis("Pitch");
+        float roll = Input.GetAxis("Roll");
+        float yaw = Input.GetAxis("Yaw");
 
-        rb.AddForce(new Vector3(lateralTrans * translationalThrust, 0, ventralTrans * translationalThrust));
+        rb.AddRelativeForce(new Vector3(lateralTrans * translationalThrust, cranialTrans * translationalThrust, ventralTrans * translationalThrust));
+        rb.AddRelativeTorque(new Vector3(pitch * rotationalThrust, yaw * rotationalThrust, roll * rotationalThrust));
+
+        rb.MoveRotation(Quaternion.LookRotation(new Vector3(gameObject.transform.rotation.x, gameObject.transform.rotation.y, 0)));
     }
 }
