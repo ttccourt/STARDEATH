528bd74688ae3b4fa6009f988ff902ae6d1434b9
iDoObject <19980290+iDoObject@users.noreply.github.com>
2020-02-23 11:02:57 +0100

    Add fuel level; track burns properly
    This also fixes a bug where burning positive along one axis and negative along another would cancel each other out and play no sound

--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class MovementController : MonoBehaviour
 {
-    public float fuel = 100;
+    public float fuelLevel = 100;
+    public float fuelMax = 100;
+    public float fuelBurnSpeed = 0.1f;
+
     public float rotationalThrust;
     public float translationalThrust;
 
@@ -34,16 +37,27 @@ public class MovementController : MonoBehaviour
         float yaw = Input.GetAxis("Yaw");
 
         if (rollDisabled) roll = 0;
-  
-        if (ventralTrans + lateralTrans + cranialTrans + pitch + roll + yaw != 0) {
-            if (!audio.isPlaying)
+
+        float absTotalImpulse = 0;
+        foreach (float direction in new float[]{ventralTrans, lateralTrans, cranialTrans, pitch, roll, yaw})
+        {
+            float absDirection = Mathf.Abs(direction);
+            if (absDirection > 0)
             {
-                audio.Play();
+                absTotalImpulse += absDirection;
+                fuelLevel -= absDirection * fuelBurnSpeed;
+                Debug.Log(fuelLevel);
+                if (!audio.isPlaying)
+                {
+                    audio.Play();
+                }
             }
-        } else
+        }
+        if (absTotalImpulse == 0)
         {
             audio.Stop();
         }
+  
 
         rb.AddRelativeForce(new Vector3(lateralTrans * translationalThrust, cranialTrans * translationalThrust, ventralTrans * translationalThrust));
         rb.AddRelativeTorque(new Vector3(pitch * rotationalThrust, yaw * rotationalThrust, roll * rotationalThrust));
