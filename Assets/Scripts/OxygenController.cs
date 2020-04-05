38a73e05b26c2bc057f6dae749c0fe84cb60d9c8
iDoObject <19980290+iDoObject@users.noreply.github.com>
2020-04-05 15:34:52 +0200

    Add OxygenController

---
+++ Assets/Scripts/OxygenController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OxygenController : MonoBehaviour
+{
+    public Text indicator;
+
+    public float startingOxyLevel = 50;
+    public float oxyPerMinute = 5;
+    public float oxyAlertThreshold = 0.1f;
+
+    private bool indicatorFlashing = false;
+
+
+    // allow OxyLevel to be read but not written by other scripts
+    public float OxyLevel { get; private set; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        OxyLevel = startingOxyLevel;
+        InvokeRepeating("DoFlashIfSet", 0, 0.333333f);
+    }
+
+    void DoFlashIfSet()
+    {
+        if (indicatorFlashing)
+        {
+            if (indicator.color == Color.white)
+            {
+                indicator.color = Color.red;
+            }
+            else
+            {
+                indicator.color = Color.white;
+            }
+        }
+
+    }
+
+    void FixedUpdate()
+    {
+        OxyLevel -= (oxyPerMinute / 60) * Time.fixedDeltaTime;
+        indicator.text = OxyLevel.ToString("000");
+    }
+
+    void Update()
+    {
+        if (OxyLevel < startingOxyLevel * oxyAlertThreshold)
+        {
+            indicatorFlashing = true;
+        }
+        else
+        {
+            indicatorFlashing = false;
+        }
+    }
+}
