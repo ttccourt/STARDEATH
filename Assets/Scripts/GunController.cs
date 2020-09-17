abe7bdd75ccc6e1018450324cd0c52d211953005
Theo Court <19980290+iDoObject@users.noreply.github.com>
2020-09-17 11:34:39 +0200

    Add weapons switching system

---
+++ Assets/Scripts/GunController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunController : MonoBehaviour
+{
+    public GameObject[] guns;
+    public GameObject selectedGun;
+
+    void SelectGun(int gun)
+    {
+        selectedGun.SetActive(false);
+        selectedGun = guns[gun];
+        selectedGun.SetActive(true);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Select Pistol"))
+        {
+            SelectGun(0);
+        }
+        if (Input.GetButtonDown("Select SMG"))
+        {
+            SelectGun(1);
+        }
+        if (Input.GetButtonDown("Select Rifle"))
+        {
+            SelectGun(2);
+        }
+        if (Input.GetButtonDown("Select Sniper"))
+        {
+            SelectGun(3);
+        }
+    }
+}
