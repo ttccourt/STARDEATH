39e419b05853d937202a76d46fcc638268da9a41
Theo Court <19980290+iDoObject@users.noreply.github.com>
2020-11-19 21:57:52 +0100

    Add crosshair

---
+++ Assets/Scripts/PlayerTagger.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTagger : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(GameObject.FindGameObjectsWithTag("Player")[0].transform);
+    }
+}
